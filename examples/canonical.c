350f549454b837b2eb3b61777066535271f7b569
abcoste2 <abcoste2@ncsu.edu>
2024-10-23 14:54:19 -0400

    examples

---
+++ examples/canonical.c
@@ -0,0 +1,15 @@
+void test(int n) {
+  int i = 0;
+  if (n > 0) {
+    do {
+      // Loop body
+      int a = 1;
+      a = 1 - a;
+      i += 1;
+    } while (i < n);
+  }
+}
+
+int main(){
+    test(5);
+}
