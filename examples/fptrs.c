2c4e5b2afe0572dfde111db35c2073071884a2c3
abcoste2 <abcoste2@ncsu.edu>
2024-12-09 00:43:22 -0500

    better indirect call example

--- examples/fptrs.c
+++ examples/fptrs.c
@@ -16,11 +16,25 @@ void print(int x, int y, int (*func)(int,int))
     printf("value is : %d\n", (*func)(x, y));
 }
 
-int main()
+int main(int argc, char *argv[])
 {
     int x=100, y=200;
     print(x,y,add);
     print(x,y,sub);
+
+    // call add if first argument is 1
+    int (*func)(int,int);
+    if(argc == 1)
+    {
+        func = add;
+    }
+    else
+    {
+        func = sub;
+    }
+
+    print(x, y, func);
+    (*func)(x, y);
 
     return 0;
 }
